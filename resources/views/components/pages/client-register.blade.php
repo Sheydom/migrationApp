be468d86e8bfa09db7d49cc6be6f44abd897537a
Dominic Knabe <dominic.knabe@gmail.com>
2026-07-30 17:59:50 +1000

    added email confirmation to client register

--- resources/views/components/pages/client-register.blade.php
+++ resources/views/components/pages/client-register.blade.php
@@ -14,6 +14,7 @@ new class extends Component {
     public string $email;
     public string $phone;
     public string $nationality;
+    public string $email_confirmation;
 
 
     public function save(): void
@@ -21,15 +22,13 @@ new class extends Component {
         $validated = $this->validate([
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:clients,email',
+            'email' => 'required|email|unique:clients,email|confirmed',
             'phone' => 'required|string|max:30',
             'nationality' => 'required|string|max:50',
         ]);
 
         Client::create($validated);
 //        $this->redirect('pages/thank-you');
-
-        
     }
 
 };
@@ -38,11 +37,32 @@ new class extends Component {
 <div>
     <h1>Client Intake Form</h1>
     <form wire:submit="save">
-        <label for="first_name">First Name<input id="first_name" type="text" wire:model="first_name"></label>
-        <label for="last_name">Last Name<input id="last_name" type="text" wire:model="last_name"></label>
-        <label for="nationality">Nationality<input id="nationality" type="text" wire:model="nationality"></label>
-        <label for="email">Email<input id="email" type="email" wire:model="email"></label>
-        <label for="phone">Phone<input id="phone" type="tel" wire:model="phone"></label>
+        <label for="first_name">First Name<input id="first_name" type="text" wire:model="first_name" required></label>
+        @error('first_name')
+        {{ $message }}
+        @enderror
+        <label for="last_name">Last Name<input id="last_name" type="text" wire:model="last_name" required></label>
+        @error('last_name')
+        {{ $message }}
+        @enderror
+        <label for="nationality">Nationality<input id="nationality" type="text" wire:model="nationality"
+                                                   required></label>
+        @error('nationality')
+        {{ $message }}
+        @enderror
+        <label for="email">Email<input id="email" type="email" wire:model="email" required></label>
+        @error('email')
+        {{ $message }}
+        @enderror
+        <label for="email_confirmation">Confirm Email Address<input id="email_confirmation" type="email"
+                                                                    wire:model="email_confirmation" required></label>
+        @error('email_confirmation')
+        {{ $message }}
+        @enderror
+        <label for="phone">Phone<input id="phone" type="tel" wire:model="phone" required></label>
+        @error('phone')
+        {{ $message }}
+        @enderror
         <button type="submit">Submit</button>
     </form>
 </div>
