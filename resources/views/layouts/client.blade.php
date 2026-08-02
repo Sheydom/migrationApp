68f1242178ff88df7ec2782b221420a0e741e865
Dominic Knabe <dominic.knabe@gmail.com>
2026-08-02 19:25:53 +1000

    client template change of title

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Solis Migration</title>
+    <title>Client intake Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
